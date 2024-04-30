139e7c4d87884172ccbf9a58cb0ad7f3dee1e6ae
Tanguy <pierrontanguy@gmail.com>
2024-04-30 11:26:31 +0200

    added second activity

--- app/src/main/java/com/example/quizz/MainActivity.kt
+++ app/src/main/java/com/example/quizz/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.quizz
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+private lateinit var m_buttonSubmit: Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        m_buttonSubmit = findViewById(R.id.buttonSubmit)
+
+        m_buttonSubmit.setOnClickListener {
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
+            startActivity(intent)
         }
     }
 }
