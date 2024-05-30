93bc2fe0b64f5c78349feb7c47e5470e08e4451e
Vladimir9595 <vladisac@hotmail.it>
2024-05-31 00:16:37 +0200

    fix : fix the callback to category fragment

--- app/src/main/java/com/example/quizz/ScoreFragment.kt
+++ app/src/main/java/com/example/quizz/ScoreFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.content.SharedPreferences
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.quizz.databinding.FragmentScoreBinding
@@ -31,34 +32,30 @@ class ScoreFragment : Fragment() {
         val username = sharedPreferences.getString("username", "Unknown") ?: "Unknown"
         val score = arguments?.getInt("score") ?: 0
 
-        //binding.scoreTextView.text = "Score: $score"
-        //binding.usernameTextView.text = "Pseudo: $username"
-
         // Save the current player's score
         savePlayerScore(username, score)
-
         // Retrieve the last 5 players' scores
         val playerScores = getLastPlayerScores(sharedPreferences)
-
-        // Display the scores
         displayPlayerScores(playerScores)
 
-        binding.buttonCategory.setOnClickListener {
-            findNavController().navigate(R.id.action_ScoreFragment_to_CategoryFragment)
-        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigate(R.id.action_ScoreFragment_to_CategoryFragment)
+            }
+        })
     }
 
     private fun savePlayerScore(username: String, score: Int) {
         val playerScores = getLastPlayerScores(sharedPreferences).toMutableList()
         if (playerScores.size >= 5) {
-            playerScores.removeAt(0) // Remove the oldest entry if we already have 5
+            playerScores.removeAt(0)
         }
         playerScores.add(PlayerScore(username, score))
         savePlayerScores(sharedPreferences, playerScores)
     }
 
     private fun displayPlayerScores(playerScores: List<PlayerScore>) {
-        val scoresText = playerScores.joinToString("\n") { "Pseudo: ${it.username} - Score: ${it.score}" }
+        val scoresText = playerScores.joinToString("\n") { "Pseudo: ${it.username} - Score: ${it.score}/10" }
         binding.scoreTextView.text = scoresText
     }
 
